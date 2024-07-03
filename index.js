2b71031a6dad98d85ad2de29847c04c6969e2fbf
Ivy Nyambura <ikagam018@gmail.com>
2024-07-03 19:31:12 +0300

    did find

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+];
+
+function superbowlWin(record){
+    const findWin = record.find(function(record){
+        return record.result === "W";
+    });
+    return  findWin ? findWin.year : undefined
+
+}
+
+console.log(superbowlWin(record));
+
